987ba04eb9462550bb7b66f9bf138f8e4098f452
Atharva Kawale <84341581+athu-kawale2002@users.noreply.github.com>
2022-03-21 22:53:13 +0530

    updated node js
    ye pull request ko accept kr aur git pull mar tere system pe, an code run kar ke dekh index.js me kadya kiya hai

--- src/index.js
+++ src/index.js
@@ -7,19 +7,20 @@ import "tachyons"
 import {robots} from './robots'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-      <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
-      <Card id={robots[5].id} name={robots[5].name} email={robots[5].email}/>
-      <Card id={robots[6].id} name={robots[6].name} email={robots[6].email}/>
-      <Card id={robots[7].id} name={robots[7].name} email={robots[7].email}/>
-      
+ <React.StrictMode>
+    <div className='tc br3 pa3 ma2 grow'>
+      <h1>The Robofriends</h1>
     </div>
-    
+    <div className='tc'>
+      <Cards id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Cards id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Cards id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      <Cards id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+      <Cards id={robots[4].id} name={robots[4].name} email={robots[4].email} />
+      <Cards id={robots[5].id} name={robots[5].name} email={robots[5].email} />
+      <Cards id={robots[6].id} name={robots[6].name} email={robots[6].email} />
+      <Cards id={robots[7].id} name={robots[7].name} email={robots[7].email} />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
